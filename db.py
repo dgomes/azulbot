0e3540b977a1e3006be8de93d330cbf89cbfd2ba
Diogo Gomes <dgomes@ua.pt>
2023-08-21 00:38:08 +0100

    optimize sync

--- db.py
+++ db.py
@@ -52,10 +52,13 @@ class Database:
     def sync_photos(self) -> None:
         with self._db as db:
             for photo in os.listdir(self._photos_dir):
-                if imghdr.what(f"{self._photos_dir}/{photo}") is None:
-                    logger.info(f"Skipping non-image file {photo}")
+                if photo.is_dir():
                     continue
                 if f"{self._photos_dir}/{photo}" not in db:
+                    if imghdr.what(f"{self._photos_dir}/{photo}") is None:
+                        logger.info(f"Skipping non-image file {photo}")
+                        continue
+
                     logger.info(f"Adding new photo {photo}")
                     db[f"{self._photos_dir}/{photo}"] = Photo(
                         count=0, last_used=0, caption="", cid="", uri=""
